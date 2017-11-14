c09c559457cf7ec8617afc6ea8ad7229214a15fb
Aleksey Soloviov <lelkaklel@gmail.com>
2017-11-14 12:56:27 +0300

    re_crop_words changed

--- re_crop_words.py
+++ re_crop_words.py
@@ -7,7 +7,7 @@
 # и заменяет весь текст на отсортированный список уникальных 
 # значений первых групп из найденных вхождений шаблона.
 
-re_template_text = r"PG_SALESISISCACHE\.([\[\]\w\d]+)"
+re_template_text = r"PG_SALESISISCACHE\.\[?([\w\d]+)\]?"
 
 re_template_text = notepad.prompt('Please, print Python regex template...', 'RegEx template', re_template_text)
 
@@ -16,13 +16,13 @@ if re_template_text:
     matches = set()
 
     def match_found(m):
-        matches.add(m.group(1))
+        matches.add(str(m.group(1)).lower())
         
     editor.research(re_template_text, match_found) 
 
     editor.clearAll()  # cleares all text in the current document
-
-    matches = sorted(matches, key=lambda s: s.lower())
+    
+    matches = sorted(matches)
 
     for m in matches:
         editor.addText('{}\n'.format(m))
